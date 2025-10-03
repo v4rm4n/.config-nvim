dbf71d25948b8b8e3e90911cb6ec5197138dca89
v4rm4n <adityavarman.manjunath@gmail.com>
2025-10-03 12:19:18 +0530

    theme update

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "craftzdog/solarized-osaka.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
